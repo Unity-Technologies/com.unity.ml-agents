Unity-Technologies/com.unity.ml-agents

commit 21fa54cd3e9f039b8b3d46d04e7e08361b1eed16

The environment can now close right after being instantiated (#4071)

com.unity.ml-agents/Runtime/Academy.cs
@@ -366,6 +366,8 @@ namespace Unity.MLAgents
                         port = port
                     }
                 );
+                Communicator.QuitCommandReceived += OnQuitCommandReceived;
+                Communicator.ResetCommandReceived += OnResetCommand;
             }
 
             if (Communicator != null)
@@ -396,12 +398,6 @@ namespace Unity.MLAgents
                         "Will perform inference instead."
                     );
                     Communicator = null;
-                }
-
-                if (Communicator != null)
-                {
-                    Communicator.QuitCommandReceived += OnQuitCommandReceived;
-                    Communicator.ResetCommandReceived += OnResetCommand;
                 }
             }
 

com.unity.ml-agents/Runtime/Communicator/RpcCommunicator.cs
@@ -214,10 +214,16 @@ namespace Unity.MLAgents
 
             m_Client = new UnityToExternalProto.UnityToExternalProtoClient(channel);
             var result = m_Client.Exchange(WrapMessage(unityOutput, 200));
-            unityInput = m_Client.Exchange(WrapMessage(null, 200)).UnityInput;
+            var inputMessage = m_Client.Exchange(WrapMessage(null, 200));
+            unityInput = inputMessage.UnityInput;
 #if UNITY_EDITOR
             EditorApplication.playModeStateChanged += HandleOnPlayModeChanged;
 #endif
+            if (result.Header.Status != 200 || inputMessage.Header.Status != 200)
+            {
+                m_IsOpen = false;
+                QuitCommandReceived?.Invoke();
+            }
             return result.UnityInput;
 #else
             throw new UnityAgentsException(

commit 7e7568f467cd358600d5b70b23c75a76cc0ee92a

Release 3 Cherry-pick bug-fixes and doc changes from master (#4102)
* [bug-fix] Fix regression in --initialize-from feature (#4086)

* Fixed text in GettingStarted page specifying the logdir for tensorboard. Before it was in a directory summaries which no longer existed. Results are now saved to the results dir. (#4085)

* [refactor] Remove nonfunctional `output_path` option from TrainerSettings (#4087)

* Reverting bug introduced in #4071 (#4101)

Co-authored-by: Scott <Scott.m.jordan91@gmail.com>
Co-authored-by: Vincent-Pierre BERGES <vincentpierre@unity3d.com>

com.unity.ml-agents/Runtime/Academy.cs
@@ -366,8 +366,6 @@ namespace Unity.MLAgents
                         port = port
                     }
                 );
-                Communicator.QuitCommandReceived += OnQuitCommandReceived;
-                Communicator.ResetCommandReceived += OnResetCommand;
             }
 
             if (Communicator != null)
@@ -399,6 +397,11 @@ namespace Unity.MLAgents
                     );
                     Communicator = null;
                 }
+            }
+            if (Communicator != null)
+            {
+                Communicator.QuitCommandReceived += OnQuitCommandReceived;
+                Communicator.ResetCommandReceived += OnResetCommand;
             }
 
             // If a communicator is enabled/provided, then we assume we are in